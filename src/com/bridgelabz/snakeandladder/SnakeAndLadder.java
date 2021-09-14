84a8e55d9b5a30d8c1a33019923fa330faa074bf
777shetty <shetty.chetu7@gmail.com>
2021-09-14 22:50:04 +0530

    ADD:ADD option for NOREPLY,SNAKE and LADDER

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,17 +1,50 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
-int position;
+	int position;
+	String name;
+	public static final int NOPLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 	
-	public SnakeAndLadder() {
+	public SnakeAndLadder(String name) {
 		this.position = 0;
+		this.name = name;
 	}
-	
-	
+	public static int rollDice() {
+		return ((int)(Math.floor(Math.random()*10)%6)+1);
+	}
+	public void play() {
+		int result = rollDice();
+		System.out.println(this.name+": Rolling Dice Value = "+result);
+		
+		int option = ((int)(Math.floor(Math.random()*10)%3));
+		switch (option) {
+		case NOPLAY:
+			result = 0;
+			System.out.println("OPTION: NOPLAY");
+			break;
+		case LADDER:
+			System.out.println("OPTION: LADDER");
+
+			break;
+		case SNAKE:
+			System.out.println("OPTION: SNAKE");
+			result = -result;
+			break;
+
+		default: System.out.println("Invalid Option!");
+			break;
+		}
+		position += result;
+		System.out.println(position);
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!");
 		System.out.println("");
-		SnakeAndLadder player1 = new SnakeAndLadder();
+		SnakeAndLadder player1 = new SnakeAndLadder("Player 1");
+		player1.play();
+		
 	}
 }
 
