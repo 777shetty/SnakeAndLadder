b63c7eddc54a0cd1940d37cfe5e1372468c8d772
777shetty <shetty.chetu7@gmail.com>
2021-09-14 22:30:58 +0530

    Initial Files

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+int position;
+	
+	public SnakeAndLadder() {
+		this.position = 0;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Game!");
+		System.out.println("");
+		SnakeAndLadder player1 = new SnakeAndLadder();
+	}
+}
+
+
